b4332edeb2219bb0810f180de15cee5dbef70abf
Shepherd Umanah <shefkid2@gmail.com>
2023-08-21 23:30:07 +0100

    Finished Product Details View

--- app/src/main/java/com/example/commerce/Activity/DetailActivity.kt
+++ app/src/main/java/com/example/commerce/Activity/DetailActivity.kt
@@ -1,11 +1,12 @@
-package com.example.commerce
+package com.example.commerce.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.commerce.R
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detail)
     }
 }
